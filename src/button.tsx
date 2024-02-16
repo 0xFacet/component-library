f69b132393598b38ecc34ca62ebb0eced2cb50b1
Michael Hirsch <michael@hirsch.me>
2024-02-15 23:27:21 -0600

    fix button

--- src/button.tsx
+++ src/button.tsx
@@ -11,73 +11,80 @@ export interface ButtonProps
   isLoading?: boolean;
 }
 
-export const Button: React.FC<ButtonProps> = ({
-  variant,
-  onClick,
-  children,
-  isLoading: isLoadingProp,
-  disabled,
-  ...props
-}) => {
-  const [isLoadingInternal, setLoading] = React.useState(false);
-  const isLoading =
-    isLoadingProp !== undefined ? isLoadingProp : isLoadingInternal;
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      variant,
+      onClick,
+      children,
+      isLoading: isLoadingProp,
+      disabled,
+      ...props
+    },
+    ref
+  ) => {
+    const [isLoadingInternal, setLoading] = React.useState(false);
+    const isLoading =
+      isLoadingProp !== undefined ? isLoadingProp : isLoadingInternal;
 
-  const handleClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
-    if (onClick) {
-      try {
-        setLoading(true);
-        await onClick(event);
-      } catch (error) {
-      } finally {
-        setLoading(false);
+    const handleClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
+      if (onClick) {
+        try {
+          setLoading(true);
+          await onClick(event);
+        } catch (error) {
+        } finally {
+          setLoading(false);
+        }
       }
-    }
-  };
+    };
 
-  const loadingIndicator = (
-    <div
-      style={{
-        position: "absolute",
-        left: "50%",
-        top: "50%",
-        transform: "translate(-50%, -50%)",
-      }}
-    >
-      <svg
-        className={`animate-spin h-4 w-4 ${
-          variant === "default" || !variant ? "text-black" : "text-white"
-        } ${isLoading ? "opacity-100" : "opacity-0"}`}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
+    const loadingIndicator = (
+      <div
+        style={{
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
       >
-        <circle
-          className="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          strokeWidth="4"
-        ></circle>
-        <path
-          className="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
-    </div>
-  );
+        <svg
+          className={`animate-spin h-4 w-4 ${
+            variant === "default" || !variant ? "text-black" : "text-white"
+          } ${isLoading ? "opacity-100" : "opacity-0"}`}
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+      </div>
+    );
 
-  return (
-    <ButtonUi.Button
-      onClick={handleClick}
-      disabled={disabled || isLoading}
-      variant={variant}
-      {...props}
-    >
-      <span style={{ opacity: isLoading ? 0 : 1 }}>{children}</span>
-      {loadingIndicator}
-    </ButtonUi.Button>
-  );
-};
+    return (
+      <ButtonUi.Button
+        ref={ref}
+        onClick={handleClick}
+        disabled={disabled || isLoading}
+        variant={variant}
+        {...props}
+      >
+        <span style={{ opacity: isLoading ? 0 : 1 }}>{children}</span>
+        {loadingIndicator}
+      </ButtonUi.Button>
+    );
+  }
+);
+Button.displayName = "Button";
